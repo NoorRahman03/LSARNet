1576b8ae420fef1e76c571c83301037f54e40c96
NoorRahman03 <mahsud03@gmail.com>
2026-07-17 20:26:28 +0500

    LSARNet_Test.m

--- Code/LSARNet_Test.m
+++ Code/LSARNet_Test.m
@@ -1,45 +1,21 @@
-
+% Load test images from the SOC dataset folder
+% Labels are automatically assigned based on subfolder names (one folder per class)
 dataset_path ='C:\Users\mahsu\OneDrive\Desktop\SARATR\Dataset\SOC1\'
 data=fullfile(dataset_path,'Test');
 testDataSOC3=imageDatastore(data,'IncludeSubfolders',true,'LabelSource','foldernames');
 
+% Load the pre-trained LSARNet model from the saved .mat file
 load LSARNet.mat
 
-% %for single File.
-% [fileName,filePath] = uigetfile('*.*', "Select an Image");
-% a=imread(fullfile(filePath,fileName));
-% %imshow(a)
-% [out,score]=classify(training,a);
-% figure,
-% imshow(a)
-% title(string(3out));
-
-
+% Run the trained model on each test image and collect predicted class labels
 allclass=[];
 for i = 1:length(testDataSOC3.Labels)
-a=readimage(testDataSOC3,i);
-%a=uint8(a); %for binary images
-out=classify(training,a);
-allclass=[allclass out];
-%figure,imshow(a);
-%title(string(out));
+  a=readimage(testDataSOC3,i);
+  out=classify(training,a);
+  allclass=[allclass out];
 end
 
+% Store predictions and plot confusion matrix to evaluate classification performance
 Predicted=allclass;
 figure,
 plotconfusion(testDataSOC3.Labels,Predicted')
-
-% sourcePath=('C:\Users\mahsu\OneDrive\Desktop\SARATR\Dataset\SOC\TestSet\BMP2\SN_9563\');
-% Files = dir([sourcePath,'*.tif']);
-% allclass=[];
-% for i = 1:length(Files)
-% a=imread([sourcePath,Files(i).name]);
-% out=classify(training,a);
-% arr(i,:)=out;
-% fname(i,:)=Files(i).name;
-% %figure,imshow(a);
-% %title(string(out));
-% end
-
-
-
